226a8f8409ab8776c822e4693de5306d01a9703a
Arthur <957980613@qq.com>
2022-09-15 16:02:36 +0800

    share data

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-python nonlinear_adp.py --run_mode 0
-
-python nonlinear_adp.py --run_mode 1
+python nonlinear_adp.py --run_mode 1 --modeltype notears --s0 40 --d 20 --n 1000 --trial 1 --seed 0
