04dd9ffcb0abed5e22b6d9ccfac173f764cdf302
Arthur <957980613@qq.com>
2022-09-29 17:13:54 +0800

    plot and readme

--- README.md
+++ README.md
@@ -1,8 +1,3 @@
-# Reweight 
+# ReScore
 
-+ synthetic data: nonlinear gp
-  + nodes: [10,20,30,40,50]
-  + mode: [ER2,3,4; SF2,3,4]
-
-+ real data: sachs 7466 and sachs 843
   
--- plot_utils.py
+++ plot_utils.py
@@ -17,6 +17,7 @@ WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEM
 COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
+from cProfile import label
 import os
 
 import matplotlib
@@ -114,50 +115,61 @@ if __name__ == '__main__':
 
     import os
     import json
-    xs=[]
-    ys={}
-    method="notears"# TODO: change this
-    temperature=40  # TODO: change this
-    reweight=f"_reweight_{temperature}"# TODO: change this
+    xs=[[],[],[],[]]
+    ys=[{},{},{},{}]
+    method="notears"
+    temperature=40
+    gf="ER"
+    reweight=f"_reweight_{temperature}"
     #reweight="_reweight"
-    setting="10_40_2000"# TODO: change this
+    setting="10_40_2000"
 
+    temperatures=[10,20,30,40]
     best_shd_base=1e10
     best_perf_base=""
     best_shd_rescore=1e10
     best_perf_rescore=""
 
-    prefix = 'rs_expt_t_40_bs_500'# TODO: change this
+    for i,temperature in enumerate(temperatures):
+        reweight=f"_reweight_{temperature}"
+        path=f"non-linear/synthetic/{setting}/{gf}_gp"
+        for file in os.listdir(path):
+            #if file.endswith(f"{method}_cut0.01{reweight}.json"):
+            if file.endswith(f"{method}{reweight}.json"):
+                # if float(file.split("_")[1])==0.015:
+                #     continue
+                if float(file.split("_")[1]) == 0.01:
+                    continue
+                xs[i].append(float(file.split("_")[1]))
+                abs_path=os.path.join(path, file)
+                with open(abs_path) as f:
+                    data=json.load(f)
+                perf_str=file.split("_")[1]+"\n"
+                flag=0
+                for key,value in data.items():
+                    if key not in ys[i]:
+                        ys[i][key]=[[],[]]
+                    ys[i][key][0].append(value['mean'])
+                    ys[i][key][1].append(value['std'])
+                    if key =="shd":
+                        if value['mean']<best_shd_rescore:
+                            best_shd_rescore=value['mean']
+                            flag=1
+                    perf_str+=f"{key}:{value['mean']}+-{value['std']}\n"
+                
+                if flag==1:
+                    best_perf_rescore=perf_str
 
-    path=f"{prefix}/linear/synthetic/{setting}/SF_gauss"# TODO: change this
-    for file in os.listdir(path):
-        #if file.endswith(f"{method}_cut0.01{reweight}.json"):
-        if file.endswith(f"{method}{reweight}.json"):
-            xs.append(float(file.split("_")[1]))
-            abs_path=os.path.join(path, file)
-            with open(abs_path) as f:
-                data=json.load(f)
-            perf_str=file.split("_")[1]+"\n"
-            flag=0
-            for key,value in data.items():
-                if key not in ys:
-                    ys[key]=[[],[]]
-                ys[key][0].append(value['mean'])
-                ys[key][1].append(value['std'])
-                if key =="shd":
-                    if value['mean']<best_shd_rescore:
-                        best_shd_rescore=value['mean']
-                        flag=1
-                perf_str+=f"{key}:{value['mean']}+-{value['std']}\n"
-            
-            if flag==1:
-                best_perf_rescore=perf_str
-    
+    print(ys)
+    print(xs)
+
     xs_ori=[]
     ys_ori={}
     for file in os.listdir(path):
         #if file.endswith(f"{method}_cut0.01.json"):
         if file.endswith(f"{method}.json"):
+            # if float(file.split("_")[1])<0.001:
+            #     continue
             xs_ori.append(float(file.split("_")[1]))
             abs_path=os.path.join(path, file)
             with open(abs_path) as f:
@@ -178,31 +190,65 @@ if __name__ == '__main__':
             if flag==1:
                 best_perf_base=perf_str
     
-    idx=np.argsort(np.array(xs))
-    idx_ori=np.argsort(np.array(xs_ori))
-    xs=np.array(xs)[idx]
+    
+    idx_ori=np.argsort(np.array(xs_ori[i]))
     xs_ori=np.array(xs_ori)[idx_ori]
+    idxs=[]
+    for i in range(4):
+        idx=np.argsort(np.array(xs[i]))
+        xs[i]=np.array(xs[i])[idx]
+        idxs.append(idx)
+    
 
     plt.figure(figsize=(16,8))
     cnt=0
-    for key,item in ys.items():
-        plt.subplot(2,3,cnt+1)
-        means=np.array(item[0])[idx]
-        stds=np.array(item[1])[idx]
-        means_ori=np.array(ys_ori[key][0])[idx_ori]
-        stds_ori=np.array(ys_ori[key][1])[idx_ori]
-        plt.errorbar(xs, means, yerr=stds,color='r',fmt='-o',label=method+"_reweight")
-        plt.errorbar(xs_ori, means_ori, yerr=stds_ori,color='b',fmt='-o',label=method)
+    for key,item in ys_ori.items():
+        plt.subplot(f"23{cnt}")
+        # means=np.array(item[0])[idx]
+        # stds=np.array(item[1])[idx]
+        # means_ori=np.array(ys_ori[key][0])[idx_ori]
+        # stds_ori=np.array(ys_ori[key][1])[idx_ori]
+
+        
+        means_ori=np.array(item[0])[idx_ori]
+        stds_ori=np.array(item[1])[idx_ori]
+
+        for i in range(4):
+
+            means=np.array(ys[i][key][0])[idxs[i]]
+            stds=np.array(ys[i][key][1])[idxs[i]]
+            p=plt.plot(xs[i], means, 'o-',label=rf"NOTEARS-MLP+ReScore, $\tau$={temperatures[i]}")
+        #plt.plot(xfit, yfit, '-', color='gray')
+            c=p[0].get_color()
+
+        #plt.xlabel(r'$\lambda$')
+        #plt.ylabel(key.upper())
+            plt.fill_between(xs[i], means - stds, means + stds,
+                            color=c, alpha=0.05)
+        
+
+        # p=plt.plot(xs_ori, means_ori, 'o-',label="NOTEARS-MLP")
+        # c=p[0].get_color()
+        # #plt.plot(xfit, yfit, '-', color='gray')
+
+        # plt.fill_between(xs_ori, means_ori - stds_ori, means_ori + stds_ori,
+        #                 color=c, alpha=0.05)
+        #plt.errorbar(xs, means, yerr=stds,color='r',fmt='-o',label=method+"_reweight")
+        #plt.errorbar(xs_ori, means_ori, yerr=stds_ori,color='b',fmt='-o',label=method)
         plt.title(key)
         plt.legend()
         cnt+=1
-    plt.suptitle(setting)
-    plt.savefig(os.path.join(path,"final.jpg"))
+    #plt.savefig(os.path.join(path,setting+f"_{key}.png"),bbox_inches='tight')
+    plt.savefig(os.path.join(path,setting+"_final.png"),bbox_inches='tight')
     plt.cla()
+    
+    # plt.suptitle(setting)
+    
 
     print("baseline:")
     print(best_perf_base)
     print("rescore:")
     print(best_perf_rescore)
+        
 
         
